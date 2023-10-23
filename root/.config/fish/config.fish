637801e6fd2c51ff354230a30770f700cd1548cb
spongeyperson <commits@spongey.xyz>
2023-10-23 04:25:39 -0700

    Pushing Branch to Untested
    Changes are ready to be staged, but have not been tested to work correctly, heance the new branch

--- root/.config/fish/config.fish
+++ root/.config/fish/config.fish
@@ -1,3 +1,11 @@
-# Aliases
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=/'
+#alias define
+alias ls="ls -h --color"
+alias cp="cp -v"
+alias mv="mv -v"
+alias rm="rm -v"
+alias mkdir="mkdir -v"
+alias permof="stat -c '%a %n'"
+alias perm="stat -c '%a %n'"
 
+#sync with proxmox config
+alias config="/usr/bin/git --git-dir=/root/.dotfiles/ --work-tree=/"
